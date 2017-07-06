ae08f2faca4a28f684e945908163a8052761af9b
3DRing <45933861qw>
2017-07-06 18:51:23 +0500

    adding another solution (sliding window): Algorithms: Implementation: Birthday Chocolate

--- src/com/ringov/algorithms/implementation/BirthdayChocolate.java
+++ src/com/ringov/algorithms/implementation/BirthdayChocolate.java
@@ -7,7 +7,11 @@ package com.ringov.algorithms.implementation;
  */
 public class BirthdayChocolate {
 
-    static int solve(int n, int[] s, int d, int m) {
+    public static int solve(int n, int[] s, int d, int m) {
+        return slidingWindowSolution(n, s, d, m);
+    }
+
+    private static int naiveSolution(int n, int[] s, int d, int m) {
         int counter = 0;
         for (int i = 0; i < n; i++) {
             int sum = 0;
@@ -16,6 +20,25 @@ public class BirthdayChocolate {
                 sum += s[j];
                 j++;
             }
+            if (sum == d) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    static int slidingWindowSolution(int n, int[] s, int d, int m) {
+        int counter = 0;
+        int sum = 0;
+        for (int i = 0; i < m; i++) {
+            sum += s[i];
+        }
+        if (sum == d) {
+            counter++;
+        }
+        for (int i = m; i < n; i++) {
+            sum -= s[i - m];
+            sum += s[i];
             if (sum == d) {
                 counter++;
             }
